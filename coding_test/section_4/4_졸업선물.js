e87243de03a1456fea5b341eac01adac7f43bd2f
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-16 19:11:44 +0900

    완전 탐색 4-5 문제 풀이

--- coding_test/section_4/4_졸업선물.js
+++ coding_test/section_4/4_졸업선물.js
@@ -12,3 +12,26 @@ let arr = [
   [10, 3],
 ];
 console.log(solution(28, arr));
+
+/////////////////////////////////////////
+function solution(m, product) {
+  let answer = 0;
+  let n = product.length;
+  product.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+  for (let i = 0; i < n; i++) {
+    // 남은 예산
+    let money = m - (product[i][0] / 2 + product[i][1]); // 할인 받은 비용
+    let cnt = 1;
+    // 남은 가격으로 살 수 있는거
+    for (let j = 0; j < n; j++) {
+      if (j !== i && product[j][0] + product[j][1] > money) break; // 답 나오면 멈춤
+      // 할인된 상품은 포함X
+      if (j !== i && product[j][0] + product[j][1] <= money) {
+        money -= product[j][0] + product[j][1];
+        cnt++;
+      }
+    }
+    answer = Math.max(answer, cnt);
+  }
+  return answer;
+}
--- coding_test/section_4/5_k번째큰수.js
+++ coding_test/section_4/5_k번째큰수.js
@@ -6,3 +6,23 @@ function solution(n, k, card) {
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
 console.log(solution(10, 3, arr));
+
+/////////////////////////////////////////
+function solution(n, k, card) {
+  let answer;
+  let tmp = new Set(); // 중복 수 제거
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        // 3개씩 모두 더함
+        tmp.add(card[i] + card[j] + card[k]);
+      }
+    }
+  }
+  // 가장 큰 수 부터 정렬
+  let a = Array.from(tmp).sort((a, b) => b - a);
+  // k 번째 수 골라냄
+  answer = a[k - 1];
+
+  return answer;
+}
