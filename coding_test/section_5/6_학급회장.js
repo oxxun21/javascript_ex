91df403c9e28e859bc64cc74dcc777520426e41a
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-22 16:21:28 +0900

    해쉬 문제풀이

--- coding_test/section_5/6_학급회장.js
+++ coding_test/section_5/6_학급회장.js
@@ -6,3 +6,20 @@ function solution(s) {
 
 let str = "BACBACCACCBDEDE";
 console.log(solution(str));
+/////////////////////////////////////////
+function solution(s) {
+  let answer;
+  let sh = new Map();
+  for (let x of s) {
+    if (sh.has(x)) sh.set(x, sh.get(x) + 1);
+    else sh.set(x, 1);
+  }
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let [key, val] of sh) {
+    if (val > max) {
+      max = val;
+      answer = key;
+    }
+  }
+  return answer;
+}
--- coding_test/section_5/7_아나그램.js
+++ coding_test/section_5/7_아나그램.js
@@ -1,9 +1,29 @@
-function solution(str1, str2) {
-  let answer = "YES";
-
-  return answer;
-}
+// function solution(str1, str2) {
+//   let answer = "YES";
+//   let sh = new Map();
+//   for (let x of str1) {
+//     if (sh.has(x)) sh.set(x, sh.get(x) + 1);
+//     else sh.set(x, 1);
+//   }
+//   for (let x of str2) {}
+//   console.log(sh, sh2);
+//   return answer;
+// }
 
 let a = "AbaAeCe";
 let b = "baeeACA";
 console.log(solution(a, b));
+/////////////////////////////////////////
+function solution(str1, str2) {
+  let answer = "YES";
+  let sh = new Map();
+  for (let x of str1) {
+    if (sh.has(x)) sh.set(x, sh.get(x) + 1);
+    else sh.set(x, 1);
+  }
+  for (let x of str2) {
+    if (!sh.has(x) || sh.get(x) === 0) return "NO";
+    sh.set(x, sh.get(x) - 1);
+  }
+  return answer;
+}
