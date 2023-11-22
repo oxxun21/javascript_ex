7f282040c956cff26169f46066e8ac6991df922e
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-22 13:55:27 +0900

    투 포인터 알고리즘 문제풀이

--- coding_test/section_5/2_공통원소구하기.js
+++ coding_test/section_5/2_공통원소구하기.js
@@ -11,3 +11,19 @@ function solution(arr1, arr2) {
 let a = [1, 3, 9, 5, 2];
 let b = [3, 2, 5, 7, 8];
 console.log(solution(a, b));
+/////////////////////////////////////////
+function solution(arr1, arr2) {
+  let answer = [];
+  let p1 = 0,
+    p2 = 0;
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1]++);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+  return answer.sort((a, b) => a - b);
+}
