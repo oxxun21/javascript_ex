3877d7893d6b3396f396d523f37700e51028f1ec
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-22 15:58:39 +0900

    슬라이딩 윈도우 문제 풀이

--- coding_test/section_5/5_최대매출.js
+++ coding_test/section_5/5_최대매출.js
@@ -1,16 +1,28 @@
-function solution(k, arr) {
-  let answer = 0;
-  let num = 0;
-  for (let i = 0; i < arr.length; i++) {
-    num = arr[i];
-    for (let j = i + 1; j < arr.length; j++) {
-      // num += arr[j];
-      console.log(arr[i], arr[j]);
-    }
-    // if (num > answer) answer = num;
-  }
-  return answer;
-}
+// function solution(k, arr) {
+//   let answer = 0;
+//   let num = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     num = arr[i];
+//     for (let j = i + 1; j < arr.length; j++) {
+//       // num += arr[j];
+//       console.log(arr[i], arr[j]);
+//     }
+//     // if (num > answer) answer = num;
+//   }
+//   return answer;
+// }
 
 let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
 console.log(solution(3, a));
+/////////////////////////////////////////
+function solution(k, arr) {
+  let answer = 0,
+    sum = 0;
+  for (let i = 0; i < k; i++) sum += arr[i];
+  answer = sum;
+  for (let i = k; i < arr.length; i++) {
+    sum += arr[i] - arr[i - k];
+    answer = Math.max(answer, sum);
+  }
+  return answer;
+}
