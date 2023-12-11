66cb1e185d0ab21a4f78b6e733e0c2c1492926f7
Oh Ga Eun <dhrkdms4113@naver.com>
2023-12-11 17:39:07 +0900

    자릿수의 합 문제풀이

--- coding_test/section_4/1_자릿수의합.js
+++ coding_test/section_4/1_자릿수의합.js
@@ -17,32 +17,11 @@
 //   return answer;
 // }
 
-let arr = [128, 460, 603, 40, 521, 137, 123];
-console.log(solution(7, arr));
+// let arr = [128, 460, 603, 40, 521, 137, 123];
+// console.log(solution(7, arr));
 
 /////////////////////////////////////////
-function solution(n, arr) {
-  let answer,
-    max = Number.MIN_SAFE_INTEGER;
 
-  for (let x of arr) {
-    let sum = 0,
-      tmp = x;
-    while (tmp) {
-      sum += tmp % 10; // 10으로 나눈 나머지로 한자리씩 쪼갤 수 있음
-      tmp = Math.floor(tmp / 10);
-    }
-    if (sum > max) {
-      max = sum;
-      answer = x;
-    } else if (sum === max) {
-      if (x > answer) answer = x;
-    }
-  }
-  return answer;
-}
-
-/////////////////////////////////////////
 function solution(n, arr) {
   let answer,
     max = Number.MIN_SAFE_INTEGER;
@@ -51,13 +30,59 @@ function solution(n, arr) {
     let sum = x
       .toString()
       .split("")
-      .reduce((a, c) => a + Number(c), 0);
-    if (sum > max) {
+      .reduce((a, c) => parseInt(a) + parseInt(c), 0);
+    if (max < sum) {
       max = sum;
       answer = x;
-    } else if (sum === max) {
+    } else if (max === sum) {
       if (x > answer) answer = x;
     }
   }
+
   return answer;
 }
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
+
+/////////////////////////////////////////
+// function solution(n, arr) {
+//   let answer,
+//     max = Number.MIN_SAFE_INTEGER;
+
+//   for (let x of arr) {
+//     let sum = 0,
+//       tmp = x;
+//     while (tmp) {
+//       sum += tmp % 10; // 10으로 나눈 나머지로 한자리씩 쪼갤 수 있음
+//       tmp = Math.floor(tmp / 10);
+//     }
+//     if (sum > max) {
+//       max = sum;
+//       answer = x;
+//     } else if (sum === max) {
+//       if (x > answer) answer = x;
+//     }
+//   }
+//   return answer;
+// }
+
+// /////////////////////////////////////////
+// function solution(n, arr) {
+//   let answer,
+//     max = Number.MIN_SAFE_INTEGER;
+
+//   for (let x of arr) {
+//     let sum = x
+//       .toString()
+//       .split("")
+//       .reduce((a, c) => a + Number(c), 0);
+//     if (sum > max) {
+//       max = sum;
+//       answer = x;
+//     } else if (sum === max) {
+//       if (x > answer) answer = x;
+//     }
+//   }
+//   return answer;
+// }
