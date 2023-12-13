4e13d84328af4e9e298224538adfd35a691f1abe
Oh Ga Eun <dhrkdms4113@naver.com>
2023-12-13 16:54:58 +0900

    PS 뒤집은 소수

--- coding_test/section_4/2_뒤집은소수.js
+++ coding_test/section_4/2_뒤집은소수.js
@@ -21,10 +21,31 @@
 //   return answer;
 // }
 
+// let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+// console.log(solution(arr));
+
+/////////////////////////////////////////
+function isPrime(num) {
+  if (num === 1) return false;
+  for (let i = 2; i <= parseInt(num / 2); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(arr) {
+  let answer = [];
+  for (let x of arr) {
+    let num = Number(x.toString().split("").reverse().join(""));
+    if (isPrime(num)) answer.push(num);
+  }
+  return answer;
+}
+
 let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
 console.log(solution(arr));
 
-/////////////////////////////////////////
+// /////////////////////////////////////////
 function isPrime(num) {
   if (num === 1) return false;
   for (let i = 2; i <= parseInt(num / 2); i++) {
